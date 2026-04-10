763577b329bb7c08a65ce6ce2bec27247ccd9248
Nikola Tasić <nikolatasic2004@gmail.com>
2026-04-10 16:28:11 +0200

    Izmenjena klasa volcano

--- Domain/Models/Volcano.cs
+++ Domain/Models/Volcano.cs
@@ -1,4 +1,6 @@
-﻿namespace Domain.Models
+﻿using System;
+
+namespace Domain.Models
 {
     public class Volcano
     {
@@ -6,20 +8,29 @@
         public string NazivVulkana { get; set; } = string.Empty;
         public string Drzava { get; set; } = string.Empty;
         public int Visina { get; set; } = 0;
+        public string PhotoPath { get; set; } = string.Empty;
+        public string RTFPath { get; set; } = string.Empty;
+        public DateTime DatumDodavanja { get; set; } = DateTime.UtcNow;
 
         public Volcano() { }
-        public Volcano(string naziv, string drzava, int visina)
+        public Volcano(string naziv, string drzava, int visina, string photopath, string rtfpath, DateTime datetime)
         {
             NazivVulkana = naziv;
             Drzava = drzava;
             Visina = visina;
+            PhotoPath = photopath;
+            RTFPath = rtfpath;
+            DatumDodavanja = datetime;
         }
-        public Volcano(long id, string naziv, string drzava, int visina)
+        public Volcano(long id, string naziv, string drzava, int visina, string photopath, string rtfpath, DateTime datetime)
         {
             Id = id;
             NazivVulkana = naziv;
             Drzava = drzava;
             Visina = visina;
+            PhotoPath = photopath;
+            RTFPath = rtfpath;
+            DatumDodavanja = datetime;
         }
     }
 }
